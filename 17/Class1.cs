0ac5df7eb2d19180c625cf673dc2427dff1baf40
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-22 01:00:04 +0300

    Добавьте файлы проекта.

---
+++ 17/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    class BankAccount<T>
+        
+    {
+        private T Num { get; set; }
+        private string Name { get; set; }
+        private double AccountBalance { get; set; }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+          Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс счета");
+            AccountBalance = Convert.ToDouble(Console.ReadLine());
+        }
+        public string GetInfo()
+        { 
+        return $"{Num} {Name} {AccountBalance}";    
+        }
+    }
+}
---
+++ 17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Net.Http;
+
+namespace _17
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BankAccount<string> account1= new BankAccount<string>();
+            account1.Input();
+            Console.WriteLine(account1.GetInfo());
+            BankAccount<int> account2 = new BankAccount<int>();
+            account2.Input();
+            Console.WriteLine(account2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+
+
+}
+
